f499ac836ad3f9fed58ae06df3797a1c376fb6bf
ck <shotkey@gmail.com>
2021-01-16 14:46:42 -0800

    reverted status change in miner.py changed title in readme made build generic with rems

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-call setpy 37
+rem personal call to setup for Python 3.7 on a system with multiple python versions installed.  comment out.
+rem call setpy 37
 pyinstaller --onefile miner.py --hidden-import pkg_resources --hidden-import infi.systray --hidden-import bleak --icon=miner_on.ico --version-file miner_version_info.txt --noconsole
 copy configuration.ini dist
 copy Miner_off.ico dist
 copy Miner_on.ico dist
-copy dist\*.* W:\temp\ethMining
+REM personal copy, can remove for your build
+rem copy dist\*.* W:\temp\ethMining
